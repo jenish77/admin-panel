053217e6b099ff2cc99247f02b94caa38828b35e
Jenish maru <jenish.m@elaunchinfotech.in>
2023-09-21 12:47:10 +0530

    add category api

---
+++ src/sections/customer/category-table.js
@@ -0,0 +1,124 @@
+import PropTypes from 'prop-types';
+import {
+  Avatar,
+  Box,
+  Card,
+  Checkbox,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
+} from '@mui/material';
+import { Scrollbar } from 'src/components/scrollbar';
+
+export const CategoryTable = (props) => {
+  const {
+    count = 0,
+    items = [],
+    onDeselectAll,
+    onDeselectOne,
+    onPageChange = () => {},
+    onRowsPerPageChange,
+    onSelectAll,
+    onSelectOne,
+    page = 0,
+    rowsPerPage = 0,
+    selected = []
+  } = props;
+
+  const selectedSome = (selected.length > 0) && (selected.length < items.length);
+  const selectedAll = (items.length > 0) && (selected.length === items.length);
+
+  return (
+    <Card>
+      <Scrollbar>
+        <Box sx={{ minWidth: 800 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedAll}
+                    indeterminate={selectedSome}
+                    onChange={(event) => {
+                      if (event.target.checked) {
+                        onSelectAll?.();
+                      } else {
+                        onDeselectAll?.();
+                      }
+                    }}
+                  />
+                </TableCell>
+                <TableCell>
+                  Image
+                </TableCell>
+                <TableCell>
+                  Category Name
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((category) => {
+                const isSelected = selected.includes(category.id);
+
+                return (
+                  <TableRow
+                    hover
+                    key={category.id}
+                    selected={isSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        checked={isSelected}
+                        onChange={(event) => {
+                          if (event.target.checked) {
+                            onSelectOne?.(category.id);
+                          } else {
+                            onDeselectOne?.(category.id);
+                          }
+                        }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Avatar src={category.image} />
+                    </TableCell>
+                    <TableCell>
+                      {category.category_name}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+      </Scrollbar>
+      <TablePagination
+        component="div"
+        count={count}
+        onPageChange={onPageChange}
+        onRowsPerPageChange={onRowsPerPageChange}
+        page={page}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+};
+
+CategoryTable.propTypes = {
+  count: PropTypes.number,
+  items: PropTypes.array,
+  onDeselectAll: PropTypes.func,
+  onDeselectOne: PropTypes.func,
+  onPageChange: PropTypes.func,
+  onRowsPerPageChange: PropTypes.func,
+  onSelectAll: PropTypes.func,
+  onSelectOne: PropTypes.func,
+  page: PropTypes.number,
+  rowsPerPage: PropTypes.number,
+  selected: PropTypes.array
+};
